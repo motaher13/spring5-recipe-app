e2b3d78f99378a16d449f481d8fb9fc9bfdf97df
motaher <motaher@revesoft.com>
2020-12-19 23:51:46 +0600

    8.15 view list of recipes

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -4,7 +4,9 @@ import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -15,20 +17,26 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
+   /* private CategoryRepository categoryRepository;
     private UnitOfMeasureRepository unitOfMeasureRepository;
 
     public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
         this.categoryRepository = categoryRepository;
         this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }*/
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        Optional<Category> categoryOptional=categoryRepository.findByDescription("American");
+    public String getIndexPage(Model model){
+        /*Optional<Category> categoryOptional=categoryRepository.findByDescription("American");
         Optional<UnitOfMeasure> unitOfMeasure=unitOfMeasureRepository.findByDescription("Teaspoon");
         System.out.println("Cat id is:"+categoryOptional.get().getId());
-        System.out.println("UOM id is:"+unitOfMeasure.get().getId());
+        System.out.println("UOM id is:"+unitOfMeasure.get().getId());*/
+        model.addAttribute("recipes",recipeService.getRecipes());
         return "index";
     }
 }
--- src/main/java/guru/springframework/domain/Difficulty.java
+++ src/main/java/guru/springframework/domain/Difficulty.java
@@ -1,6 +1,6 @@
 package guru.springframework.domain;
 
 public enum Difficulty {
-    Easy, Moderate, Hard
+    EASY, MODERATE, HARD;
 
 }
